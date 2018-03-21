46f451aa1d6c9886bf89769def13bfc3a7cee779
Rob Martin <rob.martin.5758@gmail.com>
2018-03-21 00:08:12 -0400

    html and css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,53 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var targethead = window.document.getElementsByTagName("head")[0],
+loadedSpiders = false,
+jst = window.document.createElement("script");
+jst.async = true;
+jst.type = "text/javascript";
+jst.src = "assets/javascript/bug-min.js";
+jst.onload = jst.onreadystatechange = function() {
+if (!loadedSpiders && (!this.readyState || this.readyState == 'complete')) {
+  loadedSpiders = true;
+  // start fire the JS.
+  new SpiderController();
+}
+};
+targethead.appendChild(jst);
